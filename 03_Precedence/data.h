dbc6f8bb82c5fbce6d045e770731f60eaaca08fe
zhengbeiyun <zhengbeiyun@gmail.com>
2024-07-09 11:42:13 +0000

    copy 词法解析

---
+++ 03_Precedence/data.h
@@ -0,0 +1,13 @@
+/**
+ * 在 main.c 中定义了 extern_，这使得在 data.h 中，extern_ 被替换为空白，从而实际定义了这些全局变量。
+ * 在 scan.c 中，extern_ 被替换为 extern，从而声明了这些变量。这种方式有效地管理了全局变量的声明和定义。
+ */
+
+#ifndef extern_
+    #define extern_ extern
+#endif
+
+extern_ int Line;
+extern_ int Putback;
+extern_ FILE *Infile;
+extern_ struct token Token;
---
+++ 03_Precedence/scan.c
@@ -0,0 +1,92 @@
+#include "defs.h"
+#include "data.h"
+#include "decl.h"
+
+static int next(void) {
+    int c;
+
+    if (Putback) {
+        c = Putback;
+        Putback = 0;
+        return c;
+    }
+
+    c = fgetc(Infile);
+    if ('\n' == c) {
+        Line++;
+    }
+
+    return c;
+}
+
+static void putback(int c) {
+    Putback = c;
+}
+
+static int skip(void) {
+    int c;
+
+    c = next();
+
+    while (' ' == c || '\t' == c || '\n' == c || '\r' == c || '\f' == c) {
+        c = next();
+    }
+    return c;
+}
+
+static int chrops(char *s, int c) {
+    char *p;
+
+    p = strchr(s, c);
+    return p ? p - s : -1;
+}
+
+static int scanint(int c) {
+    int k = 0;
+    int val = 0;
+
+    // 114
+    while ((k = chrops("0123456789", c)) >= 0) {
+        val = val * 10 + k;
+        c = next();
+    }
+
+    putback(c);
+    return val;
+}
+
+
+int scan(struct token *t) {
+    int c;
+
+    c = skip();
+
+    switch (c) {
+    case EOF:
+        t->token = T_EOF;
+        return 0;
+    case '+': 
+        t->token = T_PLUS;
+        break;
+    case '-': 
+        t->token = T_MINUS;
+        break;
+    case '*': 
+        t->token = T_STAR;
+        break;
+    case '/': 
+        t->token = T_SLASH;
+        break;
+    default:
+        if (isdigit(c)) {
+            t->intvalue = scanint(c);
+            t->token = T_INTLIT;
+            break;
+        }
+        printf("Unrecognised character %c on line %d\n", c, Line);
+        exit(1);
+    }
+
+    return 1;
+}
+
