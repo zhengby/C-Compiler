a8607547f2d3d7109edfcbc56c21fa95ec49e97c
zhengbeiyun <zhengbeiyun@gmail.com>
2024-07-09 06:06:57 +0000

    词法扫描

---
+++ 01_Scanner/defs.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+struct token {
+    int token;
+    int intvalue;
+};
+
+enum {
+    T_PLUS,
+    T_MINUS,
+    T_STAR,
+    T_SLASH,
+    T_INTLIT
+};
+
