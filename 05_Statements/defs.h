e8c61f63c7c5e8a6c26f807f0b3522942413ec2e
zhengbeiyun <zhengbeiyun@gmail.com>
2024-07-10 08:26:19 +0000

    copy 汇编语言生成

---
+++ 05_Statements/defs.h
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+struct token {
+    int token;
+    int intvalue;
+};
+
+enum {
+    T_EOF,
+    T_PLUS,
+    T_MINUS,
+    T_STAR,
+    T_SLASH,
+    T_INTLIT
+};
+
+enum {
+    A_ADD,
+    A_SUBTRACT,
+    A_MULTIPLY,
+    A_DIVIDE,
+    A_INTLIT
+};
+
+struct ASTnode {
+    int op;
+    struct ASTnode *left;
+    struct ASTnode *right;
+    int intvalue;
+};
+
